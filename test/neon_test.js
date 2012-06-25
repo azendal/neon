4342b3a9bae0e5c7cc09b6c52e2a0ca7d87fff3b
Rubén Medellín <chubas7@gmail.com>
2012-06-24 21:09:08 -0500

    Updated pseudo-test files

---
+++ test/neon_test.js
@@ -0,0 +1,70 @@
+if(typeof require !== 'undefined') { // We are in Node, need to require the file
+
+    console.log("Requiring neon from file");
+    var Neon = require('../neon.js');
+    console.log(Neon);
+
+    // In Coffeescript, the following construct is recommended
+    // { Class, Module, Interface } = require('neon')
+
+    var Class = Neon.Class;
+    var Module = Neon.Module;
+    var Interface = Neon.Interface;
+}
+
+
+console.log('Class          :', typeof Class);
+console.log('Module         :', typeof Module);
+console.log('Interface      :', typeof Interface);
+
+Module('Composition')({
+    moduleClassVariable : 1,
+    prototype : {
+        moduleInstanceVariable : 2
+    }
+});
+
+Interface('Contract')({
+    constructor : ['ensuredClassVariable'],
+    prototype   : ['ensuredInstanceVariable']
+});
+
+Class('BaseClass')({
+    baseClassVariable : 3,
+    prototype : {
+        baseInstanceVariable : 4
+    }
+});
+
+var AnonymousModule = Module()({
+    anonymousClassVariable : 5,
+    prototype : {
+        anonymousInstanceVariable : 6
+    }
+});
+
+Class('MyClass').inherits(BaseClass).ensures(Contract).includes(Composition, AnonymousModule)({
+    GREETING : 'HELLO',
+    inheritedClassVariable : 7,
+    ensuredClassVariable : 8,
+    prototype : {
+        inheritedInstanceVariable : 9,
+        ensuredInstanceVariable : 10,
+        init : function(argument) {
+            console.log(this.constructor.GREETING + ' ' + argument + '!');
+        }
+    }
+});
+
+var instance = new MyClass('world');
+console.log(MyClass.moduleClassVariable);
+console.log(instance.moduleInstanceVariable);
+console.log(MyClass.baseClassVariable);
+console.log(instance.baseInstanceVariable);
+console.log(MyClass.anonymousClassVariable);
+console.log(instance.anonymousInstanceVariable);
+console.log(MyClass.inheritedClassVariable);
+console.log(MyClass.ensuredClassVariable);
+console.log(instance.inheritedInstanceVariable);
+console.log(instance.ensuredInstanceVariable);
+
